d711ce21cd69b2444c313dcd6e241bc95aa90e43
optshj <optshj@gmail.com>
2024-10-19 20:13:14 +0900

    [Fix] UI 오류 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,4 +1,3 @@
-require("dotenv").config()
 const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = app => {
@@ -6,11 +5,7 @@ module.exports = app => {
         "/api",
         createProxyMiddleware({
             target: "https://openapi.naver.com",
-            changeOrigin: true,
-            onProxyReq: proxyReq => {
-                proxyReq.setHeader("X-Naver-Client-Id", process.env.REACT_APP_NAVER_CLIENT_ID)
-                proxyReq.setHeader("X-Naver-Client-Secret", process.env.REACT_APP_NAVER_CLIENT_SECRET)
-            }
+            changeOrigin: true
         })
     )
 }
