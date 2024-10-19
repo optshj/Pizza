35fcbcb86ba50e02d119384d84d57bec38782df2
optshj <optshj@gmail.com>
2024-10-19 13:34:22 +0900

    [Docs] Readme.md 변경 src/asset/charater 추가

--- src/api/Naver.tsx
+++ src/api/Naver.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default function Naver() {
     const naver = axios.create({
-        baseURL: "/v1/search",
+        baseURL: "https://openapi.naver.com/v1/search",
         headers: {
             "X-Naver-Client-Id": process.env.REACT_APP_NAVER_CLIENT_ID,
             "X-Naver-Client-Secret": process.env.REACT_APP_NAVER_CLIENT_SECRET
