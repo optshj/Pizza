aa997390d416639ed21c663418a89541e48f7f64
optshj <optshj@gmail.com>
2024-10-20 18:32:40 +0900

    [Feat] 주소수정

--- src/api/KakaoAPI.tsx
+++ src/api/KakaoAPI.tsx
@@ -7,7 +7,7 @@ export default function bookSearch() {
                 params: { query: name },
                 headers: {
                     Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_KEY}`,
-                    KA: "sdk/1.0.0 os/javascript lang/ko-KR origin/http://localhost:3000"
+                    KA: "sdk/1.0.0 os/javascript lang/ko-KR origin/https://optshj.github.io/Pizza/"
                 }
             })
     }
