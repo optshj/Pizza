d48ddedff9ae1f805c15143dced82b9cb222886b
optshj <optshj@gmail.com>
2024-10-20 18:32:04 +0900

    [Feat] API 수정

--- src/api/KakaoAPI.tsx
+++ src/api/KakaoAPI.tsx
@@ -1,18 +1,14 @@
 import axios from "axios"
 
-const Kakao = axios.create({
-    baseURL: "https://dapi.kakao.com",
-    headers: {
-        Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_KEY}`,
-        KA: "sdk/1.0.0 os/javascript lang/ko-KR origin/http://localhost:3000"
-    }
-})
-
 export default function bookSearch() {
     return {
         search: (name: string) =>
-            Kakao.get(`/v3/search/book?target=title`, {
-                params: { query: name }
+            axios.get(`/api/v3/search/book?target=title`, {
+                params: { query: name },
+                headers: {
+                    Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_KEY}`,
+                    KA: "sdk/1.0.0 os/javascript lang/ko-KR origin/http://localhost:3000"
+                }
             })
     }
 }
