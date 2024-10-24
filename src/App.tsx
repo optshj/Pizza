da8781cc0ba042c30d2b60b2a0ac4066d5e583f7
optshj <optshj@gmail.com>
2024-10-24 18:35:17 +0900

    [Fix] basename수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Login from "./pages/Login"
 
 export default function App() {
     return (
-        <BrowserRouter basename="/pizza">
+        <BrowserRouter basename="/Pizza">
             <Layout>
                 <Login />
             </Layout>
