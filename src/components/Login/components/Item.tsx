af18f9ab8ae251d651b37c42288d16351e12a0d0
optshj <optshj@gmail.com>
2024-10-27 17:02:06 +0900

    [Fix] Select페이지 레이아웃 수정

--- src/components/Login/components/Item.tsx
+++ src/components/Login/components/Item.tsx
@@ -3,9 +3,11 @@ import styled from "styled-components"
 
 import { GoCheck } from "react-icons/go"
 
-const Wrapper = styled.div<{ image: string }>`
-    width: 170px;
-    height: 170px;
+const Wrapper = styled.div<{ $image: string }>`
+    width: 40vw;
+    height: 40vw;
+    max-width: 170px;
+    max-height: 170px;
     border-radius: 33px;
     background-color: #4d4d4d;
     background-size: cover;
@@ -15,7 +17,9 @@ const Wrapper = styled.div<{ image: string }>`
     cursor: pointer;
     position: relative;
     overflow: hidden;
-    background-image: ${({ image }) => `url(${image})`};
+    background-image: ${({ $image }) => `url(${$image})`};
+    max-width: 100%; /* 최대 너비를 100%로 설정 */
+    box-sizing: border-box; /* 패딩과 보더를 포함한 너비 계산 */
 `
 const Overlay = styled.div<{ $isSelect: boolean }>`
     position: absolute;
@@ -27,7 +31,6 @@ const Overlay = styled.div<{ $isSelect: boolean }>`
     border-radius: 33px;
     transition: background-color 0.5s ease;
 `
-
 const Text = styled.div<{ $isSelect: boolean }>`
     color: #ffffff;
     font-size: 24px;
@@ -38,7 +41,6 @@ const Text = styled.div<{ $isSelect: boolean }>`
         opacity 0.5s ease,
         transform 0.5s ease;
 `
-
 const Icon = styled(GoCheck)<{ $isSelect: boolean }>`
     color: #ffffff;
     font-size: 40px;
@@ -61,7 +63,7 @@ export default function Item({ text, image }: ItemProps) {
     }
 
     return (
-        <Wrapper onClick={onClick} image={image}>
+        <Wrapper onClick={onClick} $image={image}>
             <Overlay $isSelect={isSelect} />
             <Text $isSelect={isSelect}>{text}</Text>
             <Icon $isSelect={isSelect} />
--- src/components/Login/components/NextButton.tsx
+++ src/components/Login/components/NextButton.tsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import FadeIn from "./FadeIn"
 
-const Wrapper = styled.div<{ isKeyboardVisible: boolean }>`
-    bottom: ${({ isKeyboardVisible }) => (isKeyboardVisible ? "20%" : "10%")};
+const Wrapper = styled.div`
+    bottom: 10%;
     position: absolute;
-    transition: bottom 0.3s ease;
 `
 const Text = styled(Link)`
     display: flex;
@@ -14,31 +12,18 @@ const Text = styled(Link)`
     justify-content: center;
     width: 331px;
     height: 64px;
-    background-color: ${({ theme }) => theme.color.red};
+    background-color: ${props => props.theme.color.red};
     border-radius: 13px;
     font-size: 22px;
     color: #ffffff;
 `
-
 interface NextButtonProps {
     to: string
     onClick?: () => void
 }
-
 export default function NextButton({ to, onClick }: NextButtonProps) {
-    const [isKeyboardVisible, setIsKeyboardVisible] = useState(false)
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsKeyboardVisible(window.innerHeight < window.outerHeight * 0.7)
-        }
-        window.addEventListener("resize", handleResize)
-        handleResize()
-        return () => window.removeEventListener("resize", handleResize)
-    }, [])
-
     return (
-        <Wrapper isKeyboardVisible={isKeyboardVisible}>
+        <Wrapper>
             <FadeIn delay="1.5s">
                 <Text to={to} onClick={onClick}>
                     {"다음"}
