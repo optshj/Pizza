944b61dc0ebed5dd641cf939101eea8201836e5b
이성빈 <105402944+optshj@users.noreply.github.com>
2024-10-31 01:05:34 +0900

    [Feat] 알람 추가 (#24)

--- src/components/Feed/components/Item.tsx
+++ src/components/Feed/components/Item.tsx
@@ -5,6 +5,19 @@ const Wrapper = styled.div`
     flex-direction: column;
     width: 100%;
 `
+const Header = styled.div``
+const Profile = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: #8d8d8d;
+`
 export default function Item() {
-    return <Wrapper></Wrapper>
+    return (
+        <Wrapper>
+            <Header>
+                <Profile />
+            </Header>
+        </Wrapper>
+    )
 }
